cc0b419caa53ddd008dc11b27086384b4587d77b
Nabil <nabil.gunbeyi@outlook.com>
2021-03-10 11:57:57 +0100

    Controllers aangemaakt

---
+++ src/Entity/Product.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prijs;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $aantal;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $foto;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrijs(): ?int
+    {
+        return $this->prijs;
+    }
+
+    public function setPrijs(int $prijs): self
+    {
+        $this->prijs = $prijs;
+
+        return $this;
+    }
+
+    public function getAantal(): ?int
+    {
+        return $this->aantal;
+    }
+
+    public function setAantal(int $aantal): self
+    {
+        $this->aantal = $aantal;
+
+        return $this;
+    }
+
+    public function getFoto(): ?string
+    {
+        return $this->foto;
+    }
+
+    public function setFoto(?string $foto): self
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+}
